918e24cbad44d6f7cd17a862832be110b43c6a66
dtomlinson98 <dmtomlinson@mail.fhsu.edu>
2024-04-27 22:06:05 -0500

    Allowing CORS from all endpoints

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-  "https://pickle-pale-grain.glitch.me",
+  "https://copper-paper-party.glitch.me",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
+  "*",
 ];
 
 module.exports = allowedOrigins;
